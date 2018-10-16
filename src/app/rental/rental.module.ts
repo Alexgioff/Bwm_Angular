000ef8ff20dd76a6acce94aaa19b3ee854b13007
Alexgioff <gioffre.alex@outlook.it>
2018-10-16 04:52:53 +0200

    Adss Login Register, Components, Authetinfications, Guard, and token interceptions

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgPipesModule } from 'ngx-pipes';
 import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 import { MapModule } from '../common/map/map.module';
+import { Daterangepicker } from 'ng2-daterangepicker';
 
 import { RentalComponent } from './rental.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
@@ -12,12 +13,14 @@ import { RentalListItemComponent } from './rental-list-item/rental-list-item.com
 import { RentalService} from './shared/rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 
+import { AuthGuard } from '../auth/shared/auth.guard';
+import { RentalDetailBookingComponent } from './rental-detail/rental-detail-booking/rental-detail-booking.component';
 const routes: Routes = [
   {path: 'rentals',
   component: RentalComponent,
   children: [
     {path: '', component: RentalListComponent},
-    {path: ':rentalId', component: RentalDetailComponent}
+    {path: ':rentalId', component: RentalDetailComponent, canActivate: [AuthGuard]}
   ]},
 ];
 
@@ -28,13 +31,15 @@ const routes: Routes = [
     RentalListComponent,
     RentalListItemComponent,
     RentalDetailComponent,
-    UppercasePipe],
+    UppercasePipe,
+    RentalDetailBookingComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     HttpClientModule,
     NgPipesModule,
-    MapModule],
+    MapModule,
+    Daterangepicker],
   providers: [RentalService]
 })
 
