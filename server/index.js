6bea28734674aba4be74be40e9041fdba7454f00
Alexgioff <gioffre.alex@outlook.it>
2018-10-19 02:10:47 +0200

    Add pre-deploy setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express             = require('express');
 const mongoose            = require('mongoose');
 const bodyParser          = require('body-parser');
-const config              = require('./config/dev');
+const config              = require('./config');
 const FakeDb              = require('./fake-db');
+const path = require('path');
 
 const rentalRoutes        = require('./routes/rentals');
 const userRoutes          = require('./routes/user');
@@ -10,8 +11,10 @@ const bookingRoutes       = require('./routes/bookings');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true, useCreateIndex: true })
         .then(() => {
-             const fakeDb = new FakeDb();
-            //fakeDb.seedDb();
+            if (process.env.NODE_ENV !== 'production') {
+              const fakeDb = new FakeDb();
+              //fakeDb.seedDb();
+            }
 });
 
 const app = express();
@@ -21,6 +24,17 @@ app.use(bodyParser.json());
 app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist/bwm-cli');
+  app.use(express.static(appPath));
+
+  app.get('*', function(req, res){
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
+
+
 
 const PORT = process.env.PORT || 3001;
 
--- src/app/common/map/map.component.ts
+++ src/app/common/map/map.component.ts
@@ -30,6 +30,7 @@ export class MapComponent {
       },
       () => {
         this.isPositionError = true;
+        this.ref.detectChanges();
       }
     );
   }
